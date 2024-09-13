77380086d1bfa2e93ce4dee806c5864b68c8908c
Jueuunn7 <wndmsrla1234@gmail.com>
2024-09-13 11:49:56 +0900

    Remove unuse import

--- auth/views.py
+++ auth/views.py
@@ -6,8 +6,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
 
 from config.util.exception.CustomException import CustomException
 from auth.serializers import SigninSerializer, SignupSerializer
