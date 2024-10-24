355ac5a80cb15fad6f1a2f183476b45ecb2c4300
Jueuunn7 <wndmsrla1234@gmail.com>
2024-10-25 02:25:22 +0900

    Edit ci

--- src/core/settings/settings-test.py
+++
@@ -1,9 +0,0 @@
-from core.settings.settings import *
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
