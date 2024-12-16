0814b5e577719906e3db618c6fabcde993ab39bc
Jueuunn7 <wndmsrla1234@gmail.com>
2024-12-16 17:53:46 +0900

    Edit logic

--- src/user/exception.py
+++ src/user/exception.py
@@ -3,3 +3,5 @@ from core.exception import BaseCustomException
 
 class UserException:
     UserNotFoundError = BaseCustomException(code=400, detail='User not found')
+    PasswordIsShortError = BaseCustomException(code=400, detail='Password is short then 4 chars')
+    UserExistsError = BaseCustomException(code=400, detail='User already exists')
