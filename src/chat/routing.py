2f7ad386329ea5164c0a417788f60734cfc4a4e3
Jueuunn7 <wndmsrla1234@gmail.com>
2024-12-26 18:12:45 +0900

    Edit nginx

--- src/chat/routing.py
+++ src/chat/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from chat import consumer
 
 
-urlpatterns = [
+websocket_urlpatterns = [
     path("ws/chat/<str:room_id>", consumer.ChatConsumer.as_asgi()),
 ]
--- src/core/urls.py
+++ src/core/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload', include('storage.urls')),
-    path('ws', include('chat.routing')),
+    path('ws', include('chat.urls')),
 
     path('auth', include('user.urls')),
     path('profile', include('userprofile.urls')),
