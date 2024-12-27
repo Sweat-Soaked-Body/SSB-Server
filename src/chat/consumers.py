df700b4b7cd4dbfe8131008b53c05cedf5b340f7
Jueuunn7 <wndmsrla1234@gmail.com>
2024-12-27 10:26:39 +0900

    Edit chat

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -9,57 +9,51 @@ from chat.models import Message, Room
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # room name 파싱
-        self.room_id = self.scope['url_route']['kwargs']['room_id']
-        self.channel_name = self.room_id
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = self.room_name
 
         await self.channel_layer.group_add(
-            self.room_id,
-            self.channel_name
+            self.room_group_name, self.channel_name
         )
 
         await self.accept()
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
-            self.room_id,
-            self.channel_name
+            self.room_group_name, self.channel_name
         )
 
     # 데이터 수신
     async def receive(self, text_data):
-        message = json.loads(text_data)
-
-        room = await self.get_room(self.room_id)
-
-        await self.save_message(
-            room=room,
-            message=message['message']
-        )
-
-        username = await self.get_username()
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
 
         await self.channel_layer.group_send(
-            self.room_id,
+            self.room_group_name,
             {
                 'type': 'chat_message',
-                'user': username,
+                'user': await self.get_username(),
                 'message': message
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
-        username = await self.get_username()
+
+        await self.save_message(
+            room=await self.get_room(self.room_group_name),
+            message=message
+        )
+
         data = {
             'message': message,
-            'user': username,
+            'user': await self.get_username(),
         }
         await self.send(text_data=json.dumps(data, ensure_ascii=False))
 
     @sync_to_async
-    def get_room(self, room_id):
-        room = Room.objects.filter(id=room_id).afirst()
+    def get_room(self, room_group_name):
+        room = Room.objects.filter(id=room_group_name).afirst()
         if room is None:
             raise RoomException.RoomNotFound
         return room
--- src/chat/routing.py
+++ src/chat/routing.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from chat import consumer
+from django.urls import re_path
+from chat import consumers
 
 
 websocket_urlpatterns = [
-    path("ws/chat/<str:room_id>", consumer.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)', consumers.ChatConsumer.as_asgi()),
 ]
