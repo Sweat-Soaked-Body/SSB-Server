4892ddb9a261840e47974b751cbc72960b111d1c
Jueuunn7 <wndmsrla1234@gmail.com>
2024-12-28 16:13:54 +0900

    Add diet

--- src/food/models.py
+++ src/food/models.py
@@ -6,7 +6,7 @@ from diet.models import Diet
 
 class Food(models.Model):
     service_user = models.ForeignKey(ServiceUser, on_delete=models.CASCADE, related_name='food', null=True, blank=False)
-    diet = models.ForeignKey(Diet, on_delete=models.CASCADE, related_name='diets', null=True, blank=False)
+    diet = models.ForeignKey(Diet, on_delete=models.CASCADE, related_name='food', null=True, blank=False)
     name = models.CharField(max_length=20)
     weight = models.PositiveIntegerField(default=100, blank=True)
     calories = models.PositiveIntegerField()
